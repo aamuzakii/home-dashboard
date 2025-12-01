596eefece2bb2900932d7d22705d13a3e4481b01
Abdullah Al Muzaki <aamuzakii@gmail.com>
2025-12-02 06:03:20 +0700

    finish POC prisma 7

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,25 +1,28 @@
+import { PrismaClient } from "@/prisma/generated/client";
 import { NextResponse } from "next/server";
-import { prisma } from "@/lib/prisma";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: `${process.env.DATABASE_URL}`,
+});
+const prisma = new PrismaClient({ adapter });
 
 export async function GET() {
   try {
     const user = await prisma.user.findFirst();
-    const user2 = await prisma.user.create({
-    data: {
-      name: 'Alice',
-      email: 'alice@prisma.io',
-    },
-  })
 
     if (!user) {
       return NextResponse.json({ error: "No user found" }, { status: 404 });
     }
 
-    const name = (user as any).name ?? "Anonymous";
+    const name = user.name ?? "Anonymous";
 
     return NextResponse.json({ name });
   } catch (error) {
     console.error("Error fetching user", error);
-    return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+    return NextResponse.json(
+      { error: "Internal server error" },
+      { status: 500 }
+    );
   }
 }
