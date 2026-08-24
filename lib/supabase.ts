edd0115436e5fddb5ddc7223cd0981d1e899b5c1
Abdullah Al Muzaki <aamuzakii@gmail.com>
2026-08-24 14:51:32 +0700

    test dep

--- lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-export const SUPABASE_URL =
-  process.env.NEXT_PUBLIC_SUPABASE_URL ??
-  "https://gxilidqhjdtnsnvuxjse.supabase.co";
-
-export const SUPABASE_PUBLISHABLE_KEY =
-  process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ??
-  "sb_publishable_brZJLfUERmGBgXyyrL_tzQ_XqBbtnMj";
